debe15325cf27aad703ea9dd7a7ad9137c00340a
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-26 16:23:54 -0300

    Add root controller and update routes file.

---
+++ app/controllers/api/v1/root_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::RootController < ApplicationController
+  def index
+    # root 'messages#index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :messages, only: [:index]
+      root 'root#index'
+      get '/messages/random', to: 'messages#index'
     end
   end
 end
